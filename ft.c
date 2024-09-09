60ff0c2cecd94c13c5fdadfea148250c04acf38b
Thiago Kronig <thiagokronig@gmail.com>
2024-09-08 23:13:41 -0300

    Add ft_memcpy

--- ft.c
+++ ft.c
@@ -6,7 +6,7 @@
 /*   By: thiagokronig <thiagokronig@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:46:44 by thiagokroni       #+#    #+#             */
-/*   Updated: 2024/09/08 19:49:49 by thiagokroni      ###   ########.fr       */
+/*   Updated: 2024/09/08 22:49:39 by thiagokroni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,18 @@ void	ft_memset(void *s, int c, size_t n)
 	while (i < n)
 	{
 		((unsigned char *)s)[i] = (unsigned char)c;
+		i++;
+	}
+}
+
+void	ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
 }
--- ft.h
+++ ft.h
@@ -6,7 +6,7 @@
 /*   By: thiagokronig <thiagokronig@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:45:47 by thiagokroni       #+#    #+#             */
-/*   Updated: 2024/09/08 19:46:34 by thiagokroni      ###   ########.fr       */
+/*   Updated: 2024/09/08 22:49:34 by thiagokroni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include <unistd.h>
 
 void	ft_memset(void *s, int c, size_t n);
+void	ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
